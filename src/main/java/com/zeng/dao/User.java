fcd8be766e365244c4393b84c8f98da2c084cfe9
Zeng <Zeng@DeJiang.com>
2022-05-14 19:51:03 +0800

    push commit

--- src/main/java/com/zeng/dao/User.java
+++ src/main/java/com/zeng/dao/User.java
@@ -7,5 +7,6 @@ public class User {
         System.out.println("wo ai ni3");
         System.out.println("master-test");
         System.out.println("hot-fix test");
+        System.out.println("I Love you");
     }
 }
